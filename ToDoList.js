dc8327de6a1aaec977614f93410b0a193d33c5bd
Sumaira Hafeez <144407143+Sumairahafeez@users.noreply.github.com>
2024-06-18 13:57:38 +0500

    Add files via upload

--- ToDoList.js
+++ ToDoList.js
@@ -4,7 +4,6 @@ button.addEventListener('click',()=>{
     let Data = document.getElementById("Input").value;
     let div = document.createElement("div");
     div.className = "List";
-    div.style.background = "linear-gradient(135deg, #BFF0FA 0%, #cf806c 100%)";
     div.style.fontSize = "20px";
     div.style.width = "100%";
     div.style.height = "30%";
@@ -13,11 +12,25 @@ button.addEventListener('click',()=>{
     div.style.color = "black";
     div.style.display = "flex";
     div.style.justifyContent = "center";
+    div.style.gap = "160px"
     div.style.alignItems = "center";
     div.innerHTML = Data;
+    let svg = document.createElement("img");
+    svg.className = "delete";
+    svg.style.width = "48";
+    svg.style.height = "48";
+    svg.src = "https://img.icons8.com/fluency-systems-regular/48/filled-trash.png";
+    div.appendChild(svg);
     let container = document.querySelector(".container");
     let currentheight = parseFloat(window.getComputedStyle(container).height);
     container.style.height = (currentheight+window.innerHeight*(0.1))+"px";
     container.appendChild(div);
-    
+    Isdel();
 })
+function Isdel(){
+    let del = document.getElementsByTagName("img");
+    del.addEventListener('click',()=>{
+        let div = document.querySelector(".List");
+        div.hidden = true;
+    })
+}
