d7fa2eae5485e8020fa6ce6803c5000d438d33b5
Sumaira Hafeez <144407143+Sumairahafeez@users.noreply.github.com>
2024-07-02 12:28:26 +0500

    Google homepage

---
+++ google.js
@@ -0,0 +1,42 @@
+
+
+let gmail = document.getElementById("gmail");
+let image = document.getElementById("img");
+let labs = document.getElementById("labs");
+let linkeden = document.getElementById("link");
+let github = document.getElementById("git");
+let gpt = document.getElementById("gpt");
+let gemini = document.getElementById("gem");
+let yt = document.getElementById("youtube");
+let quran = document.getElementById("Quran");
+let lms = document.getElementById("lms");
+quran.addEventListener('click',()=>{
+    window.location.href = "https://quran.com/"
+})
+lms.addEventListener('click',()=>{
+    window.location.href = "https://lms.uet.edu.pk/web/login?redirect=http%3A%2F%2Flms.uet.edu.pk%2Fweb%3F%23page%3D0%26limit%3D80%26view_type%3Dlist%26model%3Dobe.core.register%26active_id%3D3060"
+})
+yt.addEventListener('click',()=>{
+    window.location.href = "https://youtube.com/"
+})
+gemini.addEventListener('click',()=>{
+    window.location.href = "https://gemini.google.com/";
+})
+gmail.addEventListener('click',()=>{
+    window.location.href = "https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"
+})
+image.addEventListener('click',()=>{
+    window.location.href = "https://www.google.com/imghp?hl=en&tab=ri&ogbl";
+})
+labs.addEventListener('click',()=>{
+    window.location.href = "https://labs.google.com/search?source=ntp";  
+})
+linkeden.addEventListener('click',()=>{
+    window.location.href = "https://www.linkedin.com/feed/";  
+})
+github.addEventListener('click',()=>{
+    window.location.href = "https://github.com/dashboard"; 
+})
+gpt.addEventListener('click',()=>{
+    window.location.href = "https://chatgpt.com/c/79899b77-60af-486e-af71-974f5d063610";
+})
